630a6b22df8d1ed4bb6bce915e884013060e1951
[rodri7997] <[rodrigo.v.7997@gmail.com]>
2021-04-07 21:50:17 -0300

    .

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -6,8 +6,7 @@ export const Home = () => {
     return (
         <div>
             <Card title="Listo con Ant" style={{ width: 300 }}>
-                <Text type="success">Dale con todo perrito! a la gran C</Text>
-
+                <Text type="success">Bienvenidos! a la gran C</Text>
             </Card>
         </div>
     )
