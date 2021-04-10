aebc1bc53cebd30e4292af5894afbf7589866c48
jmonzani <shuanche313@gmail.com>
2021-04-10 05:04:16 -0300

    footer/navbar layout

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -1,28 +1,19 @@
-
-import { Card, Input } from 'antd'
-import Text from 'antd/lib/typography/Text'
-import { FooterLayout } from '../footer/Footer'
-import './home.less'
-
-
-
+import { Card, Input } from "antd";
+import Text from "antd/lib/typography/Text";
+import { LayoutTemplate } from "../layout/Layout";
+import "./home.less";
 
 export const Home = () => {
-
-
-    return (
-        <div>
-            {/* style={{ width: 300 }} */}
-            <Card title="Listo con Ant" className="bgColor" >
-
-                <Text type="success">Bienvenidos! a la gran C</Text>
-            </Card>            
-            <Card title="Card Name" className="hightlight">
-                <Input placeholder="Your Input" />
-                <Input placeholder="Another Input" />
-            </Card>
-            <FooterLayout/>
-           
-        </div>
-    )
-}
+  return (
+    <LayoutTemplate>
+      {/* style={{ width: 300 }} */}
+      <Card title="Listo con Ant" className="bgColor">
+        <Text type="success">Bienvenidos! a la gran C</Text>
+      </Card>
+      <Card title="Card Name" className="hightlight">
+        <Input placeholder="Your Input" />
+        <Input placeholder="Another Input" />
+      </Card>
+    </LayoutTemplate>
+  );
+};
--- client/src/components/layout/Layout.tsx
+++ client/src/components/layout/Layout.tsx
@@ -1,21 +1,21 @@
-import { Layout } from 'antd';
-import { NavBar } from '../NavBar/NavBar';
-import {FooterLayout} from '../Footer/Footer'
+import { Layout } from "antd";
+import { NavBar } from "../NavBar/NavBar";
+import { FooterLayout } from "../footer/Footer";
+import { FunctionComponent } from "react";
 
 const { Header, Footer, Content } = Layout;
 
+export const LayoutTemplate: FunctionComponent = ({ children }) => {
+  return (
+    <>
+      <Layout>
+        <NavBar /> {/*NavBar*/}
+        {children} {/*Home, Accomodations, Detail, Booking*/}
+        <FooterLayout />
+      </Layout>
+    </>
+  );
+};
 
-export function LayoutTemplate() {
 
 
-    return (
-      <>
-        <Layout>
-         <NavBar/>   {/*NavBar*/}
-          <Content></Content>       {/*Home, Accomodations, Detail, Booking*/}
-        <FooterLayout/> 
-        </Layout>
-      </>
-    )
-
-}
