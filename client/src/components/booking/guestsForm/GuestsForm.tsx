248123d0ef33b69fc1df742f8729208bda6ef72b
Hector Ruiz <hruiz.com@gmail.com>
2021-04-12 12:28:13 -0500

    sin css

--- client/src/components/booking/guestsForm/GuestsForm.tsx
+++ client/src/components/booking/guestsForm/GuestsForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'antd/dist/antd.css';
+//import 'antd/dist/antd.css';
 import '../guestsForm/GuestsForm.less'
 import {
   Form,
@@ -23,47 +23,47 @@ export const GuestsForm = () => {
   };
 
   return (
-      <div className = 'conteiner'>
+    <div className='conteiner'>
 
-      <h1 className= 'adultsandchildren'> Guests </h1>
-    <Form
-      name="validate_other"
-      {...formItemLayout}
-      onFinish={onFinish}
-      initialValues={{
-        'input-number-adults': 1,
-        'input-number-children': 0,
-      }}
-    >
-        <div className = 'inputs'>
+      <h1 className='adultsandchildren'> Guests </h1>
+      <Form
+        name="validate_other"
+        {...formItemLayout}
+        onFinish={onFinish}
+        initialValues={{
+          'input-number-adults': 1,
+          'input-number-children': 0,
+        }}
+      >
+        <div className='inputs'>
 
-      <Form.Item className='input'>
-        <Form.Item label="Adults" name="input-number-adults"  >
-          <InputNumber min={1} max={6} />
-        </Form.Item>
-      </Form.Item>
+          <Form.Item className='input'>
+            <Form.Item label="Adults" name="input-number-adults"  >
+              <InputNumber min={1} max={6} />
+            </Form.Item>
+          </Form.Item>
 
-      <Form.Item className='input' >
-        <Form.Item label="Children" name="input-number-children" >
-          <InputNumber min={0} max={6} />
-        </Form.Item>
-      </Form.Item>
+          <Form.Item className='input' >
+            <Form.Item label="Children" name="input-number-children" >
+              <InputNumber min={0} max={6} />
+            </Form.Item>
+          </Form.Item>
         </div>
 
         <div className='btn'>
-      <Form.Item
-        wrapperCol={{
-            span: 12,
-            offset: 6,
-        }}
-        >
+          <Form.Item
+            wrapperCol={{
+              span: 12,
+              offset: 6,
+            }}
+          >
 
-        <Button style={{marginLeft:'30px'}} type="primary" htmlType="submit">
-          UPGRADE GUESTS
+            <Button style={{ marginLeft: '30px' }} type="primary">
+              UPGRADE GUESTS
         </Button>
-      </Form.Item>
-          </div>
-    </Form>
+          </Form.Item>
+        </div>
+      </Form>
     </div>
   );
 };
