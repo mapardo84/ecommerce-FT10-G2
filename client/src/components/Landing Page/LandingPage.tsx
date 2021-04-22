3d940f38310052f341196ea01f391606fd6e1b4b
DavidBarona <davidbaronamurcia@gmail.com>
2021-04-22 12:26:23 -0500

    prueba responsive

--- client/src/components/Landing Page/LandingPage.tsx
+++ client/src/components/Landing Page/LandingPage.tsx
@@ -1,4 +1,4 @@
-import { Row, Col, Button, Typography } from "antd";
+import {  Button, Typography } from "antd";
 import './styles.less'
 import LogIn from "../LogIn/LogIn";
 
@@ -7,29 +7,29 @@ const { Title, Text } = Typography;
 export const LandingPage = () => {
     return (
         <div className='LandingPage'>
-            <Row align="middle" justify="space-around">
 
-                <Col span={14}>
 
                     <div className='landingleft'>
 
                         <Title className='landingTitle'>HENRY HOTEL</Title>
 
                         <Text className='landingText' >
-                            A luxurious hotel, open the door to a whole new world. Feel the difference and prepare for a beautiful traveling experience.
+                            A luxurious hotel, open the door to a whole 
+                            new world. Feel the difference and prepare for a 
+                            beautiful traveling experience.
                         </Text><br />
-
-                        <Button className='lanButton' type="primary" href="/home" size="large">Continue as guest</Button>
+                        
                     </div>
-                </Col>
 
-                <Col span={10}>
                     <div className='lanForm'>
                             <LogIn />
                     </div>
-                </Col>
 
-            </Row>
+                    <div className='containbtn'>
+                        <Button className='lanButton' type="primary" href="/home" size="large">Continue</Button>
+                        </div>
+
+           
         </div>
     )
 }
