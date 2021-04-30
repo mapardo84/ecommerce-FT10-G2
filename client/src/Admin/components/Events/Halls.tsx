43e65b6fb62f03f4935361d98c1883f6a1613ecf
Mauricio Pardo Varon <mapardo19@gmail.com>
2021-04-30 17:55:56 -0500

    estado halls creado con el flujo de actions y reducer listo

--- client/src/Admin/components/Events/Halls.tsx
+++ client/src/Admin/components/Events/Halls.tsx
@@ -2,6 +2,7 @@ import { Button, Table, Form, Modal, Input, InputNumber, Tooltip, Popconfirm } f
 import { FaTrashAlt, FaPencilAlt } from 'react-icons/fa';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { getAllHalls } from '../../actions/adminEventsActions';
 
 export interface IHalls {
     id:number;
@@ -27,10 +28,123 @@ export const Halls = () => {
     const [ fields, setFields ] = useState<IFields[]>(campos);
     const { halls } = useSelector((state: any) => state?.adminEvents.halls);
     const dispatch = useDispatch();
+    
+    useEffect(() => {
+        dispatch(getAllHalls());
+    }, [dispatch]);
 
+    const handleEdit = ( id:number ) => {
+        setIsModalVisible(true)
+        const index = halls.find((halls: IHalls) => halls.id === id)
+        setEditId(index)
+        setFields([
+            { name: ['name'], value: index.name },
+            { name: ['description'], value: index.description },
+            { name: ['image'], value: index.image },
+        ])
+    }
+
+    const closeModal = () => {
+        setFields(campos)
+        setEditId(null)
+        setIsModalVisible(false)
+    }
+
+    const onFinish = (values: IHalls) => {
+        if (editId) {
+            const data = { ...values, id: editId.id }
+            // dispatch(updateType(data))
+            setIsModalVisible(false)
+            setEditId(null)
+        } else {
+            // dispatch(addType(values))
+            setIsModalVisible(false)
+        }
+    }
+
+    const handleDelete = (id: number) => {
+        const index = halls.find((type: IHalls) => type.id === id)
+        // dispatch(deleteType(index.id))
+    };
+
+    const columns:any = [
+        {
+            title: 'Type Name',
+            dataIndex: 'name',
+            key: 'name',
+            sorter: (a:IHalls, b:IHalls) => a.name.length - b.name.length,
+        },
+        {
+            title: 'Description',
+            dataIndex: 'description',
+            key: 'description'
+        },
+        {
+            title: 'Image',
+            dataIndex: 'image',
+            key: 'image',
+        }, {
+            title: 'Action',
+            dataIndex: 'operation',
+            key: 'name',
+            render: (_: undefined, record: { id: number }) =>
+                halls.length >= 1 ? (
+                    <>
+                        <Tooltip title="Edit">
+                            <span className='adminrooms_options' onClick={() => handleEdit(record.id)}><FaPencilAlt size="18" color="orange" /> </span>
+                        </Tooltip>
+                        <Tooltip title="Delete">
+                            <span className='adminrooms_options'>
+                                <Popconfirm placement="left" title="Sure to delete?" onConfirm={() => handleDelete(record.id)}>
+                                    <FaTrashAlt size="18" color="red" />
+                                </Popconfirm>
+                            </span>
+                        </Tooltip>
+                    </>
+                ) : null,
+        }
+    ]
     return (
         <div>
-            <h1>hola</h1>
+            <div className="halls_upbar">
+                <Button type="primary" onClick={() => setIsModalVisible(true)} >Add Hall</Button>
+            </div>
+            <Table
+                dataSource={halls}
+                columns={columns}
+                pagination={{ position: ['bottomCenter'] }}
+                rowKey="name"
+            />
+            <Modal title="Add Type" visible={isModalVisible} onCancel={closeModal} footer={null} >
+                <Form onFinish={onFinish} fields={fields}>
+                    <Form.Item
+                        label="Name"
+                        name="name"
+                        rules={[{ required: true, message: 'Please input a name!' }]}>
+                        <Input placeholder="name"></Input>
+                    </Form.Item>
+                    <Form.Item
+                        label="Description"
+                        name="description"
+                        rules={[{ required: true, message: 'Please input a description!' }]}>
+                        <InputNumber placeholder="description"></InputNumber>
+                    </Form.Item>
+                    <Form.Item
+                        label="Image"
+                        name="image"
+                        rules={[{ required: true, message: 'Please input an url' }]}>
+                        <InputNumber placeholder="image"></InputNumber>
+                    </Form.Item>
+                    <div className="types_btn">
+                        <Button onClick={closeModal}>
+                            Cancel
+                        </Button>
+                        <Button type="primary" htmlType="submit">
+                            Save
+                        </Button>
+                    </div>
+                </Form>
+            </Modal>
         </div>
     );
 };
--- client/src/reducers/adminEventsReducer.tsx
+++ client/src/reducers/adminEventsReducer.tsx
@@ -1,13 +1,14 @@
 import { GET_ALL_HALLS } from '../Admin/actions/adminEventsActions';
 
 const initialState = {
+    requests: [],
     halls: []
 }
 
 export const adminEventsReducer = (state = initialState, action:any) => {
     switch (action.type) {
         case GET_ALL_HALLS: 
-            return { ...state, request: action.payload };
+            return { ...state, halls: action.payload };
         default: 
             return state;
     }
