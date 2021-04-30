f583ed1b646ae29adaeead9d618afce6e1f4ac71
Mauricio Pardo Varon <mapardo19@gmail.com>
2021-04-30 08:34:28 -0500

    events del panel admin iniciado

--- client/src/routers/AppRouter.tsx
+++ client/src/routers/AppRouter.tsx
@@ -17,19 +17,18 @@ import { RoomsLayout } from "../Admin/components/RoomsLayout";
 import { CategoriesLayout } from "../Admin/components/CategoriesLayout";
 import { TypesLayout } from "../Admin/components/TypesLayout";
 import { UsersLayout } from "../Admin/components/UsersLayout";
-import Wishlist from "../components/WishList/Wishlist";
 import {WishlistLayout} from "../components/layout/WishlistLayout";
 import ResetPasswordLayout from "../components/layout/ResetPasswordLayout";
 import { MyBookingsLayout } from "../components/layout/MyBookingsLayout";
 import { MyProfileLayout } from "../components/layout/MyProfileLayout";
-import { SearchBooking } from "../Admin/components/SearchBar/SearchBar";
 import { CheckinLayout } from '../Admin/components/CheckinLayout';
 import {PaxesLayout} from '../Admin/components/PaxesLayout';
 import { BookingsLayout } from "../Admin/components/BookingsLayout";
 import {DiscountsLayout} from "../Admin/components/DiscountsLayout"
 import { SuccessLayout } from "../components/layout/SuccesLayout";
 import { Pre_booking } from "../components/Pre_booking/Pre_booking";
-import {EventLayout} from "../components/layout/EventsLayout";
+import { EventLayout } from "../components/layout/EventsLayout";
+import { EventsAdminLayout } from "../Admin/components/EventsAdminLayout";
 
 //ACA VAN TODAS LAS RUTAS
 
@@ -48,9 +47,7 @@ export const AppRouter = () => {
           <Route exact path="/myProfile" component={MyProfileLayout} />
           <Route exact path="/myBookings" component={MyBookingsLayout} />
           <Route exact path="/wishlist" component={WishlistLayout} />
-
           <Route exact path="/pre" component={Pre_booking} />
-
 
           {/* Parte administrativa */}
           <Route exact path="/admin" component={Plantilla} />
@@ -62,6 +59,7 @@ export const AppRouter = () => {
           <Route exact path="/admin/paxes" component={PaxesLayout} />
           <Route exact path="/admin/bookings" component={BookingsLayout} />
           <Route exact path="/admin/discounts" component={DiscountsLayout}/>
+          <Route exact path="/admin/events" component={EventsAdminLayout}/>
           {/* Parte administrativa */}
 
           <Route exact path="/accomodations" component={AccomodationLayout} />
